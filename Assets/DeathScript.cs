1d20dab299f819d5aa3121ff69e6baf799a98fdb
owen-mcmanus <56615953+owen-mcmanus@users.noreply.github.com>
2020-03-14 19:06:04 -0700

    Add health bar

---
+++ Assets/DeathScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    [HideInInspector]
+    public int health = 20;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(((20-health)*.15f)-8.15f,4.55f,-1f);
+    }
+}
